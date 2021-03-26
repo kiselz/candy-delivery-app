c3dbf6e9994b4b2e099eac197132332f378d7308
Aleksandr <kiselevalexander23@gmail.com>
2021-03-26 17:54:37 +0100

    Add order views

---
+++ candy_delivery_app/server/orders/views.py
@@ -0,0 +1,21 @@
+"""Orders views"""
+from flask import request
+from flask import Blueprint
+
+
+blueprint = Blueprint('orders', __name__)
+
+
+@blueprint.route('/orders', methods=('POST',))
+def load_orders():
+    pass
+
+
+@blueprint.route('/orders/assign', methods=('POST',))
+def assign_orders():
+    pass
+
+
+@blueprint.route('/orders/complete', methods=('POST',))
+def complete_order():
+    pass
