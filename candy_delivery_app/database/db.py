58d3b346761c2d8fcee31620eec7df913e5d65da
Aleksandr <kiselevalexander23@gmail.com>
2021-03-27 00:10:51 +0100

    New functions to interact with database

--- candy_delivery_app/database/db.py
+++ candy_delivery_app/database/db.py
@@ -21,6 +21,23 @@ def init_db():
         db.executescript(file.read().decode('utf-8'))
 
 
+def row_exists(table_name, unique_name, unique_value):
+    """
+    Check whether the same row already exist in the table.
+    unique_name - name of an unique column
+    unique_value - value of an unique column
+    """
+
+    db = get_db()
+    cur = db.execute('SELECT * FROM {} WHERE {} = {}'.format(
+        table_name, unique_name, unique_value
+    ))
+    data = cur.fetchall()
+    if len(data) == 0:
+        return False
+    return True
+
+
 def insert(table_name, *args):
     """
     Insert into the table a new row with all required properties.
@@ -41,6 +58,46 @@ def insert(table_name, *args):
     cur.close()
 
 
+def update(table_name, unique_name, unique_value, **kwargs):
+    """
+    Update in the table already existed row
+    unique_name - name of an unique column
+    unique_value - value of an unique column
+    kwargs - properties to update
+    """
+
+    db = get_db()
+    cur = db.execute(
+        'UPDATE {table_name} SET {kwargs} WHERE {unique_name} = {unique_value}'.format(
+            table_name=table_name,
+            kwargs=','.join(['{} = {}'.format(key, value) for key, value in kwargs.items()]),
+            unique_name=unique_name,
+            unique_value=unique_value
+        )
+    )
+    db.commit()
+    cur.close()
+
+
+def delete(table_name, column_name, column_value):
+    """
+    Delete in the table all rows that has unique_name equal to unique value
+    column_name - name of an unique column
+    column_value - value of an unique column
+    """
+
+    db = get_db()
+    cur = db.execute(
+        'DELETE FROM {table_name} WHERE {column_name} = {column_value}'.format(
+            table_name=table_name,
+            column_name=column_name,
+            column_value=column_value,
+        )
+    )
+    db.commit()
+    cur.close()
+
+
 def get_courier_type_id(courier_type):
     """
     Get courier type id based on courier type
@@ -54,12 +111,53 @@ def get_courier_type_id(courier_type):
     return courier_type_id
 
 
+def update_courier(courier):
+    """
+    Update already existed courier in the database
+    """
+
+    # Update the row in the 'courier' table
+    # key - property name, value - new value of the property
+    properties = {
+        'id': courier['courier_id'],
+        'courier_type_id': get_courier_type_id(courier['courier_type']),
+    }
+    update('courier', 'id', courier['courier_id'], **properties)
+
+    # Delete all rows in the 'couriers_regions' table with courier_id
+    delete('couriers_regions', 'courier_id', courier['courier_id'])
+
+    # Create new rows in the 'couriers_regions' table
+    for region in set(courier['regions']):
+        # args = (courier_id, region)
+        args = (courier['courier_id'], region,)
+        insert('couriers_regions', *args)
+
+    # Delete all rows in the 'couriers_working_hours' table with courier_id
+    delete('couriers_working_hours', 'courier_id', courier['courier_id'])
+
+    # Create new rows in the 'couriers_working_hours' table
+    for working_hour in set(courier['working_hours']):
+        # args = (courier_id, time(work_start), time(work_end))
+
+        work_start, work_end = working_hour.split('-')
+        args = (courier['courier_id'],
+                '"{}"'.format(work_start),
+                '"{}"'.format(work_end),
+                )
+        insert('couriers_working_hours', *args)
+
+
 def create_courier(courier):
     """
     Create new courier in the database
     """
 
-    # Create a new row in the 'couriers' table
+    if row_exists('courier', 'id', courier['courier_id']):
+        update_courier(courier)
+        return
+
+    # Create a new row in the 'courier' table
     # args = (courier_id, courier_type_id, rating, earnings)
     args = (courier['courier_id'],
             get_courier_type_id(courier['courier_type']),
@@ -69,13 +167,13 @@ def create_courier(courier):
     insert('courier', *args)
 
     # Create new rows in the 'couriers_regions' table
-    for region in courier['regions']:
+    for region in set(courier['regions']):
         # args = (courier_id, region)
         args = (courier['courier_id'], region,)
         insert('couriers_regions', *args)
 
     # Create new rows in the 'couriers_working_hours' table
-    for working_hour in courier['working_hours']:
+    for working_hour in set(courier['working_hours']):
         # args = (courier_id, time(work_start), time(work_end))
 
         work_start, work_end = working_hour.split('-')
