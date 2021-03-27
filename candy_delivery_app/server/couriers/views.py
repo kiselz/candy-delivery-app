2ad31c0b56ca55a29320d7352ffd9dc044d9ca35
Aleksandr <kiselevalexander23@gmail.com>
2021-03-27 03:05:00 +0100

    Add /couriers/id handler for patch request

--- candy_delivery_app/server/couriers/views.py
+++ candy_delivery_app/server/couriers/views.py
@@ -3,9 +3,11 @@ from flask import request
 from flask import jsonify
 from flask import make_response
 from flask import Blueprint
-from candy_delivery_app.database.db import create_courier
-from candy_delivery_app.database.db import row_exists
+import candy_delivery_app.database.db as db
 from .validation import has_all_parameters
+from .validation import has_bad_property
+from .logic import to_courier_row
+from .logic import to_courier_request
 
 blueprint = Blueprint('couriers', __name__)
 
@@ -17,10 +19,10 @@ def load_couriers():
     bad_couriers = []
     valid_couriers = []
 
-    couriers = data['data']
+    couriers = list(map(to_courier_row, data['data']))
     for courier in couriers:
         if has_all_parameters(courier):
-            if row_exists('courier', 'id', courier['courier_id']):
+            if db.row_exists('courier', 'id', courier['id']):
                 # This courier is already in the table
                 # There is another handler to update it
                 bad_couriers.append(courier)
@@ -34,27 +36,42 @@ def load_couriers():
             jsonify({
                 'validation_error': {
                     'couriers':
-                        [{'id': courier['courier_id']}
+                        [{'id': courier['id']}
                          for courier in bad_couriers]
                 }
             }), 400
         )
     else:
         for courier in valid_couriers:
-            create_courier(courier)
+            db.create_courier(courier)
 
         return make_response(
             jsonify({
                 'couriers':
-                    [{'id': courier['courier_id']}
+                    [{'id': courier['id']}
                      for courier in valid_couriers]
             }), 201
         )
 
 
 @blueprint.route('/couriers/<int:courier_id>', methods=('GET', 'PATCH',))
-def action_with_courier():
+def action_with_courier(courier_id):
     if request.method == 'PATCH':
-        pass
+        properties_to_update = request.get_json()
+        courier = db.get_courier(courier_id)
+        if len(courier) == 0 or has_bad_property(courier):
+            return make_response(
+                jsonify(
+                    {}
+                )
+            ), 400
+        courier.update(properties_to_update)
+        db.update_courier(courier)
+
+        courier = to_courier_request(courier)
+
+        return make_response(
+            jsonify(courier), 200
+        )
     elif request.method == 'GET':
         pass
